727f3d359dcded731168634b7fd79f34366938d6
Anastasiya <anastasiya.koshelevskaya@nova-test.ru>
2021-10-08 19:00:20 +0300

    feat: add styles

--- src/js/modules/proceedForm.js
+++ src/js/modules/proceedForm.js
@@ -12,6 +12,7 @@ export default {
     portDOWN: $('.js-portDOWN'),
   },
   error: '.js-error',
+  parent: '.gateway-element',
 
   init() {
     const self = this;
@@ -33,42 +34,100 @@ export default {
 
   validateGateway(val, elem) {
     const self = this;
-    const { el } = self;
+    const { error, parent } = self;
+    elem.siblings(error).text('');
+    elem.closest(parent).removeClass('_error');
 
     if (!val.match(/^(?=.*[A-Fa-f0-9])[A-Fa-f0-9]{16,}$/g)) {
-      elem.siblings(el.error).text('Поле должно содержать 16 символов');
-    } else elem.siblings(el.error).text('');
+      elem.siblings(error).text('Поле должно содержать 16 символов');
+      elem.closest(parent).addClass('_error');
+    } else {
+      elem.siblings(error).text('');
+      elem.closest(parent).removeClass('_error');
+    }
   },
 
   validatePort(val, elem) {
     const self = this;
-    const { error } = self;
+    const { error, parent } = self;
+    elem.siblings(error).text('');
+    elem.closest(parent).removeClass('_error');
 
     if (val < 0 || val > 65535) {
       elem.siblings(error).text('Значение должно быть не более 65535');
-    } else elem.siblings(error).text('');
+      elem.closest(parent).addClass('_error');
+    } else {
+      elem.siblings(error).text('');
+      elem.closest(parent).removeClass('_error');
+    }
   },
 
   handleSubmit() {
     const self = this;
-    const { el } = self;
+    const { error, parent } = self;
+    console.log($(parent));
+    $(error).text('');
+    $(parent).each((index, value) => {
+      $(value).removeClass('_error');
+    });
 
-    console.log(el.form.serialize());
+    if (self.validation() > 0) {
+      //
+    }
   },
 
   validation() {
     const self = this;
-    const { el, error } = self;
+    const { el, error, parent } = self;
     let errors = 0;
 
     if (!el.select.value) {
       errors += 1;
       el.select.siblings(error).text('Выберите значение');
+      el.select.closest(parent).addClass('_error');
     }
 
     if (!el.gateway.value) {
       errors += 1;
-      el.select.siblings(error).text('Выберите значение');
+      el.gateway.siblings(error).text('Поле обязательно к заполнению');
+      el.gateway.closest(parent).addClass('_error');
+    }
+
+    if (!el.gateway.value?.match(/^(?=.*[A-Fa-f0-9])[A-Fa-f0-9]{16,}$/g)) {
+      console.log(el.gateway.value?.match(/^(?=.*[A-Fa-f0-9])[A-Fa-f0-9]{16,}$/g));
+      errors += 1;
+      el.gateway.siblings(error).text('Поле должно содержать 16 символов');
+      el.gateway.closest(parent).addClass('_error');
+    }
+
+    if (!el.serverAddr.value) {
+      errors += 1;
+      el.serverAddr.siblings(error).text('Поле обязательно к заполнению');
+      el.serverAddr.closest(parent).addClass('_error');
+    }
+
+    if (!el.portUP.value) {
+      errors += 1;
+      el.portUP.siblings(error).text('Поле обязательно к заполнению');
+      el.portUP.closest(parent).addClass('_error');
+    }
+
+    if (el.portUP.value || el.portDOWN.value) {
+      errors += 1;
+      el.portUP.siblings(error).text('Значение должно быть не более 65535');
+      el.portUP.closest(parent).addClass('_error');
+    }
+
+    if (!el.portDOWN.value) {
+      errors += 1;
+      el.portDOWN.siblings(error).text('Поле обязательно к заполнению');
+      el.portDOWN.closest(parent).addClass('_error');
+    }
+
+    if (el.portDOWN.value || el.portDOWN.value) {
+      errors += 1;
+      el.portDOWN.siblings(error).text('Значение должно быть не более 65535');
+      el.portDOWN.closest(parent).addClass('_error');
     }
 
     return errors;
